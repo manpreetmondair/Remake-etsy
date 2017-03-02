3f01cef5026b775e9c01ddf8b283e33d532187a7
MANPREET <mondair.manpreet@gmail.com>
2017-03-01 22:34:56 -0500

    final changes

--- js/global.js
+++ js/global.js
@@ -1,122 +1,50 @@
-// createCard({
-//     src: 'http://unsplash.it/400/300?image=500',
-//     caption: 'Unknown Image'
-// });
+var items = [];
 
-// createCard({
-//     src: 'http://unsplash.it/400/300?image=600',
-//     caption: 'Mystery'
-// });
+document.querySelector('#searchbutton').addEventListener('click', getSearchResults);
+document.querySelector('#search').addEventListener('keypress', function(e) {
+    // console.log(e);
+    if (e.key === 'Enter') {
+        getSearchResults();
+    }
+});
 
-// createCard({
-//     src: 'http://unsplash.it/400/300?image=700',
-//     caption: 'Something'
-// });
+getSearchResults();
 
-// createCard({
-//     src: 'http://unsplash.it/400/300?image=800',
-//     caption: 'Something'
-// });
-// createCard({
-//     src: 'http://unsplash.it/400/300?image=900',
-//     caption: 'Something'
-// });
-// createCard({
-//     src: 'http://unsplash.it/400/300?image=1000',
-//     caption: 'Something'
-// });
-// createCard({
-//     src: 'http://unsplash.it/400/300?image=500',
-//     caption: 'Something'
-// });
+function getSearchResults() {
+    var searchTerm = document.querySelector('#search').value;
 
-createCard({
-    src: './img/image2.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image4.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
-createCard({
-    src: './img/image1.jpg',
-    caption: 'Taracea Chess Set - free shipping',
-    writer: 'ElGranio',
-    price: '$156.00'
-});
+    if (searchTerm !== '') {
+        fetch('https://thinksaydo.com/tiyproxy.php?https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent(searchTerm) + '&includes=Images,Shop')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            items = data.results;
+            createResultCards();
+        });
+    }
+}
+
+function createResultCards() {
+    document.querySelector('#cards').innerHTML = '';
+
+    // Loop over the users array, one by one, passing each user into our function as "user"
+    items.forEach(function(item) {
+       createCard(item);
+    });
+}
 
 function createCard(image) {
     var card = `<div class="col-sm-4 col-md-3">
         <div class="card">
-        <img src="${image.src}" alt="Image" />
-           <p class="card-caption">${image.caption}</p>
-           <ul li class="writername col-sm-6">${image.writer}</ul>
-            <ul li class="prices col-sm-3 col-sm-offset-2">${image.price}</ul>
+        <img src="${image.Images[0].url_75x75}" alt="Image" />
+           <p class="card-caption">${image.title.slice(0,20)}...</p>
+            <div>
+                    <span class="text-muted">${image.Shop.shop_name}</span>
+                    <span class="text-success pull-right">${'$'}${image.price}</span>
+                </div>
            
         </div>
     </div>`;
-    //  <ul class="list-inline">
-    //                                 <li class="col-sm-6">ghsgdhs</li>
-    //                                 <li class="col-sm-4 col-sm-offset-2">gsfgd</li>
-    //                                 </ul>
 
     document.querySelector('#cards').innerHTML += card; 
 }
